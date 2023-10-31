d7d3a06e89822204a2ca5bb593fa1e5d6d958500
xymaa <warrior-curry@sjtu.edu.cn>
2023-10-31 16:45:21 +0800

    231031-01

--- model/networks/basic_block.py
+++ model/networks/basic_block.py
@@ -1,4 +1,36 @@
 import torch
 import torch.nn as nn
 
-class 
+class Activation(nn.Module):
+    def __init__(self, act_type, **params):
+        super(Activation,self).__init__()
+        if act_type == 'relu':
+            self.act = nn.ReLU()
+        elif act_type == 'leaky_relu':
+            self.act = nn.LeakyReLU(**params)
+        else:
+            raise ValueError(act_type)
+
+    def forward(self, x):
+        return self.act(x)
+
+class MLP(nn.Module):
+    def __init__(self, layer_num, in_size, hidden_size, out_size, act, dropout):
+        super(MLP, self).__init__()
+
+        layers = nn.ModuleList()
+        for layer_id in range(layer_num):
+            if layer_id == 0:
+                layers.append(nn.Linear(in_size, hidden_size))
+                layers.append(nn.Dropout(dropout))
+                layers.append(Activation(act))
+            elif layer_id < layer_num-1:
+                layers.append(nn.Linear(hidden_size, hidden_size))
+                layers.append(nn.Dropout(dropout))
+                layers.append(Activation(act))
+            else:
+                layers.append(nn.Linear(hidden_size, out_size))
+        self.mlp = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.mlp(x)

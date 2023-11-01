9b89ca925b90ba15994ee24bc10be08f02473844
xymaa <warrior-curry@sjtu.edu.cn>
2023-11-01 11:30:03 +0800

    231101-01

--- model/networks/gnn_block.py
+++ model/networks/gnn_block.py
@@ -16,14 +16,62 @@ class GraphNorm(nn.Module):
         super(GraphNorm, self).__init__()
         self.graph_pool = SumPooling()
 
-    def forward(self, graph:DGLHeteroGraph, feature):
+    def forward(self, graph:DGLHeteroGraph, feature): # feature:N*32
         nodes = torch.ones(size=[graph.num_nodes(), 1]).to(my_device)
         norm = self.graph_pool(graph, nodes) # 1
         norm = torch.sqrt(norm) # 1
         norm = torch.repeat_interleave(norm, graph.batch_num_nodes()) # N
         norm = norm.unsqueeze(-1) # N*1
-        norm = norm.repeat(1, int(feature.size()[1])) # N*feature.shape[1]
+        norm = norm.repeat(1, int(feature.size()[1])) # N*32
         return feature/norm
 
+# N个节点，M个feature
+# 返回M*1
+class MeanPool(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.graph_pool = SumPooling()
 
+    def forward(self, graph, node_feat):
+        sum_pooled = self.graph_pool(graph, node_feat)
+        ones_sum_pooled = self.graph_pool(
+            graph,
+            torch.ones_like(node_feat))
+        pooled = sum_pooled / ones_sum_pooled
+        return pooled
+
+# 通过节点和边的特征得到节点的特征
+class GIN(nn.Module):
+    """
+    Implementation of Graph Isomorphism Network (GIN) layer with edge features
+    """
+    def __init__(self, hidden_size):
+        super(GIN, self).__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size * 2),
+            nn.ReLU(),
+            nn.Linear(hidden_size * 2, hidden_size))
+
+    def forward(self, graph : DGLHeteroGraph, node_feat, edge_feat):
+        graph.ndata['h'] = node_feat
+        graph.edata['e'] = edge_feat
+        graph.update_all(fn.u_add_e('h', 'e', 'm'), fn.sum('m', 'h'))
+        graph = graph.to(my_device)
+        node_feat = graph.ndata['h']
+        node_feat = self.mlp(node_feat)
+        return node_feat
+
+class GATLayer(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super(GATLayer, self).__init__()
+        self.fc = nn.Linear(in_dim, out_dim, bias=False)
+        self.attn_fc = nn.Linear(2 * out_dim, 1, bias=False)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        gain = nn.init.calculate_gain("relu")
+        nn.init.xavier_normal_(self.fc.weight, gain=gain)
+        nn.init.xavier_normal_(self.attn_fc.weight, gain=gain)
+
+    def edge_attention(self, edges):
 
--- model/networks/test.py
+++ model/networks/test.py
@@ -4,25 +4,23 @@ from dgl.nn.pytorch.glob import SumPooling
 import torch
 import torch.nn as nn
 import networkx as nx
+import dgl.function as fn
 import dgl
 import torch
 import numpy as np
 import scipy.sparse as spp
 import matplotlib.pyplot as plt
 
-u, v = torch.tensor([4,3,0,1,2,1,0,2]), torch.tensor([1,2,3,4,4,3,4,1])
-graph = dgl.graph((u, v))
-# nx.draw(g3.to_networkx(), with_labels=True)
-# plt.show()
-nodes = torch.ones(size=[graph.num_nodes(), 1])
 sp = SumPooling()
-norm = sp(graph, nodes)
-print(norm)
-# norm_1 = torch.sqrt(norm)
-# norm_2 = torch.repeat_interleave(norm_1, graph.batch_num_nodes())
-# norm_3 = norm_2.unsqueeze(-1)
-# norm_4 = norm_3.repeat(1, 4)
-# # print(nodes)
-# feature = torch.randn((6, 4))
-# res = norm_4/feature
-# print(res.shape)
+grapg = dgl.rand_graph(3, 4)
+node_feat = torch.randn((3,2))
+edge_feat = torch.randn((4,2))
+# print(node_feat)
+# print(edge_feat)
+grapg.ndata['h'] = node_feat
+grapg.edata['e'] = edge_feat
+grapg.update_all(fn.u_add_e('h', 'e', 'm'),fn.sum('m', 'h'))
+print(grapg.ndata)
+print(grapg.edata)
+print(grapg.update_all)
+

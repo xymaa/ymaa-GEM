940a3ebc107608a9ab785b683ab1837cffb37124
xymaa <warrior-curry@sjtu.edu.cn>
2023-11-02 17:18:59 +0800

    231102-02

--- model/networks/multihead_attention.py
+++ model/networks/multihead_attention.py
@@ -30,7 +30,74 @@ class SelfMultiheadAttention(nn.Module):
         bsz, tgt_len, embed_dim = query.size()
         assert embed_dim == self.embed_dim
 
+        q, k, v = self.in_proj(query).chunk(3, dim=-1)
 
+        q = (
+                q.view(bsz, tgt_len, self.num_heads, self.head_dim)
+                .transpose(1, 2)
+                .contiguous()
+                .view(bsz * self.num_heads, -1, self.head_dim)
+                * self.scaling
+        )
+        if k is not None:
+            k = (
+                k.view(bsz, -1, self.num_heads, self.head_dim)
+                .transpose(1, 2)
+                .contiguous()
+                .view(bsz * self.num_heads, -1, self.head_dim)
+            )
+        if v is not None:
+            v = (
+                v.view(bsz, -1, self.num_heads, self.head_dim)
+                .transpose(1, 2)
+                .contiguous()
+                .view(bsz * self.num_heads, -1, self.head_dim)
+            )
+        assert k is not None
 
+        src_len = k.size(1)
 
+        if key_padding_mask is not None and key_padding_mask.dim() == 0:
+            key_padding_mask = None
 
+        if key_padding_mask is not None:
+            assert key_padding_mask.size(0) == bsz
+            assert key_padding_mask.size(1) == src_len
+
+        attn_weights = torch.bmm(q, k.transpose(1, 2))
+        assert list(attn_weights.size()) == [bsz * self.num_heads, tgt_len, src_len]
+
+        if key_padding_mask is not None:
+            # don't attend to padding symbols
+            attn_weights = attn_weights.view(bsz, self.num_heads, tgt_len, src_len)
+            attn_weights.masked_fill_(
+                key_padding_mask.unsqueeze(1).unsqueeze(2).to(torch.bool), float("-inf")
+            )
+            attn_weights = attn_weights.view(bsz * self.num_heads, tgt_len, src_len)
+
+        if not return_attn:
+            attn =self.dropout(self.softmax(attn_weights))
+        else:
+            attn_weights += attn_bias
+            attn =self.dropout(self.softmax(attn_weights))
+
+        if not return_attn:
+            attn =self.dropout(self.softmax(attn_weights))
+        else:
+            attn_weights += attn_bias
+            attn =self.dropout(self.softmax(attn_weights))
+
+        o = torch.bmm(attn, v)
+        assert list(o.size()) == [bsz * self.num_heads, tgt_len, self.head_dim]
+
+        o = (
+            o.view(bsz, self.num_heads, tgt_len, self.head_dim)
+            .transpose(1, 2)
+            .contiguous()
+            .view(bsz, tgt_len, embed_dim)
+        )
+        o = self.out_proj(o)
+        if not return_attn:
+            return o
+        else:
+            return o, attn_weights, attn
--- model/networks/test.py
+++ model/networks/test.py
@@ -11,16 +11,26 @@ import numpy as np
 import scipy.sparse as spp
 import matplotlib.pyplot as plt
 
-sp = SumPooling()
-grapg = dgl.rand_graph(3, 4)
-node_feat = torch.randn((3,2))
-edge_feat = torch.randn((4,2))
-# print(node_feat)
-# print(edge_feat)
-grapg.ndata['h'] = node_feat
-grapg.edata['e'] = edge_feat
-grapg.update_all(fn.u_add_e('h', 'e', 'm'),fn.sum('m', 'h'))
-print(grapg.ndata)
-print(grapg.edata)
-print(grapg.update_all)
+# sp = SumPooling()
+# grapg = dgl.rand_graph(3, 4)
+# node_feat = torch.randn((3,2))
+# edge_feat = torch.randn((4,2))
+# # print(node_feat)
+# # print(edge_feat)
+# grapg.ndata['h'] = node_feat
+# grapg.edata['e'] = edge_feat
+# grapg.update_all(fn.u_add_e('h', 'e', 'm'),fn.sum('m', 'h'))
+# print(grapg.ndata)
+# print(grapg.edata)
+# print(grapg.update_all)
+
+attn = torch.randn((3,4,5))
+mask = torch.tensor([[[0],[1],[0],[1]],
+                     [[0],[1],[0],[1]],
+                     [[0],[1],[0],[1]]])
+# mask.unsqueeze(1)
+# print(mask.shape)
+print(mask.shape)
+attn.masked_fill_(mask.to(torch.bool), float('-inf'))
+print(attn)
 
